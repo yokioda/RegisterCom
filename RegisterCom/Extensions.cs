ea150819afdeeb6a2c2faf365186d755a6a57274
yokioda <yokioda@gmx.net>
2022-09-11 16:20:43 +0200

    Add files via upload

---
+++ RegisterCom/Extensions.cs
@@ -0,0 +1,224 @@
+﻿using WixSharp;
+using WixSharp.CommonTasks;
+
+namespace WixSharp
+{
+    //
+    // Zusammenfassung:
+    //     Collection of generic WixSharp extension methods
+    public static class Extensions
+    {
+        /// <summary>
+        /// Registers a file in the registry (e.g. *.dll, *.ocx).<br/>
+        /// <br/>
+        /// It utilizes heat.exe to extract the registry entries from the file and adds them 
+        /// to the build (*.wxs) file.<br/>
+        /// Files that do not have any registration entries stay as they are.<br/>
+        /// <br/>
+        /// The following are examples on how to register files:<br/>
+        /// <br/>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for a given file.<br/>
+        /// <code>
+        /// new File(@"Some\File\Location")
+        ///         .RegisterCom();
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for a given file as COM objects that are easier to read.<br/>
+        /// Can lead to Candle errors because heat.exe sometimes leaves stuff empty.<br/>
+        /// <code>
+        /// new File(@"Some\File\Location")
+        ///         .RegisterCom(true);
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Runs heat.exe with the additional argument '-gg' and omits the default arguments '-ag' and '-svb6'.<br/>
+        /// <code>
+        /// new File(@"Some\File\Location")
+        ///         .RegisterCom(heatArguments: new[] { "-gg" }, overrideDefaults: true);
+        /// </code>
+        /// This comes down to the same as:<br/>
+        /// <code>
+        /// new File(@"Some\File\Location")
+        ///         .RegisterCom(false, new[] { "-gg" }, true);
+        /// </code>
+        /// Or:<br/>
+        /// <code>
+        /// new File(@"Some\File\Location",
+        ///     new RegisterCom()
+        ///     {
+        ///         HeatArguments = new[] { "-gg" },
+        ///         OverrideDefaults = true
+        ///     }
+        /// );
+        /// </code>
+        /// </example>
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="file">The file to register.</param>
+        /// <param name="createComObjects">
+        /// Whether to create the output as COM objects or plain registry entries (adds argument '-scom' if false).<br/>
+        /// COM objects are easier to read but can lead to Candle errors because heat.exe sometimes leaves stuff empty.
+        /// </param>
+        /// <param name="heatArguments">Additional arguments that should be passed to heat.exe.</param>
+        /// <param name="overrideDefaults">Omits the default arguments '-ag' and '-svb6' from the call to heat.exe if further customization is wanted.</param>
+        /// <param name="hideWarnings">Whether to ignore the warnings returned by heat.exe and to stop forwarding them to the console.</param>
+        public static File RegisterCom(this File file, bool createComObjects = false, string[] heatArguments = null, bool overrideDefaults = false, bool hideWarnings = false)
+        {
+            file.Add(new RegisterCom()
+            {
+                CreateComObjects    = createComObjects,
+                HeatArguments       = heatArguments,
+                OverrideDefaults    = overrideDefaults,
+                HideWarnings        = hideWarnings
+            });
+
+            return file;
+        }
+
+        /// <summary>
+        /// Registers multiple files in the registry (e.g. *.dll, *.ocx).<br/>
+        /// <br/>
+        /// It utilizes heat.exe to extract the registry entries from the files and adds them 
+        /// to the build (*.wxs) file.<br/>
+        /// Files that do not have any registration entries stay as they are.<br/>
+        /// <br/>
+        /// The following are examples on how to register files:<br/>
+        /// <br/>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for given files.<br/>
+        /// <code>
+        /// new Files(@"Some\File\Location\*.*")
+        ///         .RegisterCom();
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for given files as COM objects that are easier to read.<br/>
+        /// Can lead to Candle errors because heat.exe sometimes leaves stuff empty.<br/>
+        /// <code>
+        /// new Files(@"Some\File\Location\*.*")
+        ///         .RegisterCom(true);
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Runs heat.exe with the additional argument '-gg' and omits the default arguments '-ag' and '-svb6'.<br/>
+        /// <code>
+        /// new Files(@"Some\File\Location\*.*")
+        ///         .RegisterCom(heatArguments: new[] { "-gg" }, overrideDefaults: true);
+        /// </code>
+        /// This comes down to the same as:<br/>
+        /// <code>
+        /// new Files(@"Some\File\Location\*.*")
+        ///         .RegisterCom(false, new[] { "-gg" }, true);
+        /// </code>
+        /// Or:<br/>
+        /// <code>
+        /// new Files(@"Some\File\Location\*.*")
+        /// {
+        ///     OnProcess = file =>
+        ///     {
+        ///         file.RegisterCom(false, new[] { "-gg" }, true);
+        ///     }
+        /// };
+        /// </code>
+        /// </example>
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="files">The files to register.</param>
+        /// <param name="createComObjects">
+        /// Whether to create the output as COM objects or plain registry entries (adds argument '-scom' if false).<br/>
+        /// COM objects are easier to read but can lead to Candle errors because heat.exe sometimes leaves stuff empty.
+        /// </param>
+        /// <param name="heatArguments">Additional arguments that should be passed to heat.exe.</param>
+        /// <param name="overrideDefaults">Omits the default arguments '-ag' and '-svb6' from the call to heat.exe if further customization is wanted.</param>
+        /// <param name="hideWarnings">Whether to ignore the warnings returned by heat.exe and to stop forwarding them to the console.</param>
+        public static Files RegisterCom(this Files files, bool createComObjects = false, string[] heatArguments = null, bool overrideDefaults = false, bool hideWarnings = false)
+        {
+            files.OnProcess = file =>
+            {
+                file.RegisterCom(createComObjects, heatArguments, overrideDefaults, hideWarnings);
+            };
+
+            return files;
+        }
+
+        /// <summary>
+        /// Registers multiple files in the registry (e.g. *.dll, *.ocx).<br/>
+        /// <br/>
+        /// It utilizes heat.exe to extract the registry entries from the files and adds them 
+        /// to the build (*.wxs) file.<br/>
+        /// Files that do not have any registration entries stay as they are.<br/>
+        /// <br/>
+        /// The following are examples on how to register files:<br/>
+        /// <br/>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for given files.<br/>
+        /// <code>
+        /// new DirFiles(@"Some\File\Location\*.*")
+        ///         .RegisterCom();
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Creates the corresponding registry entries for given files as COM objects that are easier to read.<br/>
+        /// Can lead to Candle errors because heat.exe sometimes leaves stuff empty.<br/>
+        /// <code>
+        /// new DirFiles(@"Some\File\Location\*.*")
+        ///         .RegisterCom(true);
+        /// </code>
+        /// </example>
+        /// 
+        /// <example>
+        /// Runs heat.exe with the additional argument '-gg' and omits the default arguments '-ag' and '-svb6'.<br/>
+        /// <code>
+        /// new DirFiles(@"Some\File\Location\*.*")
+        ///         .RegisterCom(heatArguments: new[] { "-gg" }, overrideDefaults: true);
+        /// </code>
+        /// This comes down to the same as:<br/>
+        /// <code>
+        /// new DirFiles(@"Some\File\Location\*.*")
+        ///         .RegisterCom(false, new[] { "-gg" }, true);
+        /// </code>
+        /// Or:<br/>
+        /// <code>
+        /// new DirFiles(@"Some\File\Location\*.*")
+        /// {
+        ///     OnProcess = file =>
+        ///     {
+        ///         file.RegisterCom(false, new[] { "-gg" }, true);
+        ///     }
+        /// };
+        /// </code>
+        /// </example>
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="dirFiles">The files to register.</param>
+        /// <param name="createComObjects">
+        /// Whether to create the output as COM objects or plain registry entries (adds argument '-scom' if false).<br/>
+        /// COM objects are easier to read but can lead to Candle errors because heat.exe sometimes leaves stuff empty.
+        /// </param>
+        /// <param name="heatArguments">Additional arguments that should be passed to heat.exe.</param>
+        /// <param name="overrideDefaults">Omits the default arguments '-ag' and '-svb6' from the call to heat.exe if further customization is wanted.</param>
+        /// <param name="hideWarnings">Whether to ignore the warnings returned by heat.exe and to stop forwarding them to the console.</param>
+        public static DirFiles RegisterCom(this DirFiles dirFiles, bool createComObjects = false, string[] heatArguments = null, bool overrideDefaults = false, bool hideWarnings = false)
+        {
+            dirFiles.OnProcess = file =>
+            {
+                file.RegisterCom(createComObjects, heatArguments, overrideDefaults, hideWarnings);
+            };
+
+            return dirFiles;
+        }
+    }
+}
---
+++ RegisterCom/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using WixSharp;
+using WixSharp.CommonTasks;
+
+class Program
+{
+    static void Main()
+    {
+        var project =   new Project("MyProduct",
+                            new Dir(@"%ProgramFiles%\My Company\My Product",
+                                // This shows how to register COM files like .dll or .ocx without the need to depend
+                                // on external programs like regsvr32.exe or the use of the frowned upon self registration.
+                                //
+                                // It utilizes heat.exe from the WiX toolkit to extract the registration data and
+                                // adds it to the installers .wxs file, therefore eliminating the need to go through
+                                // each file manually to add the entries to the generated .wxs file.
+                                //
+                                //
+                                // CreateComObjects - Used to create better readable entries with TypeLib and ProgId
+                                // entries but is more prone to cause errors, therefore disabled by default.
+                                // You can compare the two files in the .wxs to have a look for yourself.
+                                //
+                                // HeatArguments - If you want to customize the call to heat.exe.
+                                // You can find the available arguments at:
+                                // https://wixtoolset.org/documentation/manual/v3/overview/heat.html
+                                //
+                                // OverrideDefaults - Omits the default arguments for further customization.
+                                // By default heat.exe is called with '-ag' and '-svb6'
+                                //
+                                // HideWarnings - If you want to hide the warnings received from heat.exe.
+                                // You can also pass the argument '-sw<N>' to suppress all or specific warnings.
+
+
+                                // Register a single File.
+                                // Either use the extension,
+                                new File(@"Files\CSScriptLibrary.dll").RegisterCom(),
+                                // or create a new WixEntity.
+                                new File(@"Files\CSScriptLibrary2.dll",
+                                    new RegisterCom()
+                                    {
+                                        CreateComObjects = true,
+                                        HeatArguments = new[] { "-gg" },
+                                        OverrideDefaults = true,
+                                        HideWarnings = true
+                                    }
+                                )/*,
+
+                                
+                                // You can also register multiple files using either Files() or DirFiles().
+                                // Again using the extension which does the work for you,
+                                new Files(@"Files\*.*").RegisterCom(true),
+                                // or by adding it to each file manually.
+                                new Files(@"Files\*.*")
+                                {
+                                    // OnProcess is called for each file when it is actually created
+                                    // and can be used to make changes to the individual files.
+                                    OnProcess = file =>
+                                    {
+                                        // Here you can either use the extension or add a WixEntity again.
+                                        file.Add(
+                                            new RegisterCom()
+                                            {
+                                                HideWarnings = true
+                                            }
+                                        );
+                                    }
+                                },
+                                // This is literally what the extension does for Files() and DirFiles().
+                                new DirFiles(@"Files\*.*")
+                                {
+                                    OnProcess = file =>
+                                    {
+                                        file.RegisterCom();
+                                    }
+                                }
+                                */
+                            )
+                        );
+
+        project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+        project.PreserveTempFiles = true;
+
+        project.BuildMsi();
+    }
+}
